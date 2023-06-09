2c91b80368b726dde34ab1b2686a2cefabf6fc3f
Yassir Issoughaine <yassirissoughaine@gmail.com>
2023-06-09 02:19:57 -1100

    sign in with email and phone number

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,31 +22,43 @@ class UserSeeder extends Seeder
                 'nom' => 'syk',
                 'prenom' => 'web',
                 'email' => 'sykweb@gmail.com',
+                'tel' => '0661619735',
                 'password' => Hash::make('sykweb1234'),
             ],
             [
                 'nom' => 'Ziti',
                 'prenom' => 'Ilyas',
                 'email' => 'ziti.ilyas@gmail.com',
+                'tel' => '0661619734',
                 'password' => Hash::make('demo0000'),
             ],
             [
                 'nom' => 'Gamani',
                 'prenom' => 'Zakaria',
                 'email' => 'gamani.zakaria@gmail.com',
+                'tel' => '0661619733',
                 'password' => Hash::make('demo0000'),
             ],
             [
                 'nom' => 'prof x1',
                 'prenom' => 'prof x1',
                 'email' => 'prof.x1@gmail.com',
+                'tel' => '0661619732',
                 'password' => Hash::make('demo0000'),
             ],
             [
                 'nom' => 'prof x2',
                 'prenom' => 'prof x2',
                 'email' => 'prof.x2@gmail.com',
+                'tel' => '0661619731',
                 'password' => Hash::make('demo0000'),
+            ],
+            [
+                'nom' => 'Test',
+                'prenom' => 'Login',
+                'email' => 'testlogin@gmail.com',
+                'tel' => '0661619730',
+                'password' => Hash::make('loginlogin'),
             ],
         ]);
     }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,8 +14,9 @@
                                     <form action="{{route('login')}}" method="POST">
                                         @csrf
                                         <div class="form-group">
-                                            <label><strong>Email</strong></label>
-                                            <input type="email" class="form-control" name="email">
+                                            <label><strong>Email / Phone</strong></label>
+                                            <input type="text" class="form-control" name="email_or_phone" id="email-input" onkeyup="checkEmail()">
+                                            <span id="email-error"></span>
                                         </div>
                                         <div class="form-group">
                                             <label><strong>Password</strong></label>
@@ -38,70 +39,3 @@
     </div>
 </div>
 @endsection
-{{-- <div class="row justify-content-center">
-    <div class="col-md-8">
-        <div class="card">
-            <div class="card-header">{{ __('Login') }}</div>
-
-            <div class="card-body">
-                <form method="POST" action="{{ route('login') }}">
-                    @csrf
-
-                    <div class="row mb-3">
-                        <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                        <div class="col-md-6">
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                            @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="row mb-3">
-                        <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                        <div class="col-md-6">
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                            @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="row mb-3">
-                        <div class="col-md-6 offset-md-4">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                <label class="form-check-label" for="remember">
-                                    {{ __('Remember Me') }}
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row mb-0">
-                        <div class="col-md-8 offset-md-4">
-                            <button type="submit" class="btn btn-primary">
-                                {{ __('Login') }}
-                            </button>
-
-                            @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            @endif
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div> --}}
