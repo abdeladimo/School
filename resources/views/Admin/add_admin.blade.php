201694b7139832ee72198ab3e9a88eb12a2bac6c
el kaabaoui soufiane <elkaabaoui.soufiane@gmail.com>
2023-06-10 19:13:12 +0100

    link Employee with an Admiin

--- resources/views/Admin/add_admin.blade.php
+++ resources/views/Admin/add_admin.blade.php
@@ -100,6 +100,7 @@
                                             <input type="file" class="dropify" data-default-file="" name="image">
                                         </div>
                                     </div>
+                                    <x-employee-infos :employee="(isset($admin)) ? $admin->employee : new App\Models\Employee" />
                                     <div class="col-lg-12 col-md-12 col-sm-12">
                                         <button type="submit" class="btn btn-primary">Submit</button>
                                         <button type="submit" class="btn btn-light">Cancel</button>
